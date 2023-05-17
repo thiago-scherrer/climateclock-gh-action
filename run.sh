a0a73bf058d31b29def87e718c4cd6b617d48e40
thiagoscherrer <46422437+thiago-scherrer@users.noreply.github.com>
2023-05-16 23:55:23 -0300

    chore: refactored to improve readability

--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 
 cd /app
 
-wkhtmltoimage --transparent --javascript-delay ${JS_DELAY} \
-    --format png --quality 0 clock.html clock.png
+wkhtmltoimage \
+    --transparent \
+    --javascript-delay "${JS_DELAY}" \
+    --format png \
+    --quality 0 \
+    clock.html clock.png
